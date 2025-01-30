cb361a6dbe31a76dd13f4485870aafece5d42b22
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-01-30 20:39:39 +0100

    Improvements

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,34 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const axios = require("axios");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const db = admin.firestore();
 
 exports.import = onRequest(async (_, response) => {
-  const characters = [];
-
   try {
+    let counter = 0;
     let nextUrl = "https://rickandmortyapi.com/api/character";
 
     while (nextUrl) {
       console.log(`Fetching ${nextUrl}`);
       const apiResponse = await axios.get(nextUrl);
-      const json = apiResponse.data;
-      characters.push(...json.results);
-      nextUrl = json.info.next;
+      counter += apiResponse.data.results.length;
+
+      const batch = db.batch();
+
+      for (const character of apiResponse.data.results) {
+        const docRef = db.collection("characters").doc(character.id.toString());
+        batch.set(docRef, character);
+      }
+
+      const res = await batch.commit();
+      console.log(res);
+
+      nextUrl = apiResponse.data.info.next;
     }
 
-    response.send(`Imported ${characters.length} characters`);
+    response.send(`Imported ${counter} characters`);
   } catch (error) {
     console.error(error);
     response.status(500).send();
